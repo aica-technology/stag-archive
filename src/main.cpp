e7544ac616b048dfe0c46bbbad354e6c5bbf154b
Enrico Eberhard <enrico.eberhard@epfl.ch>
2022-08-11 15:20:32 +0200

    Keep hard tab in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
 {
 	cv::Mat image = cv::imread("/path/to/examples/STag-HD23.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
-  stag::Stag stag(23, 7, true);
+	stag::Stag stag(23, 7, true);
 
 	stag.detectMarkers(image);
 	stag.logResults("/path/to/examples/output/");

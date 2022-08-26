b857319809df1e6174569cae862364408d2cb314
Dominic Reber <dominic.reber@epfl.ch>
2022-08-26 14:30:48 +0200

    -j option for make command

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ apt-get install python3-opencv libopencv-dev -y
 ldconfig
 
 mkdir build && cd build || exit 1
-cmake .. && make && make install || exit 1
+cmake .. && make -j && make install || exit 1
 cd .. || exit 1
 rm -r build

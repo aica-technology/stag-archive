db7f06c59bc1d0e3102444b680bda16f84ae1d28
domire8 <71256590+domire8@users.noreply.github.com>
2022-08-26 16:24:41 +0200

    Add installation of OpenCV to install script (#4)
    * Add installation of OpenCV to install script
    
    * -j option for make command

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+## Install OpenCV
+apt-get update
+apt-get install python3-opencv libopencv-dev -y
+ldconfig
+
 mkdir build && cd build || exit 1
-cmake .. && make && make install || exit 1
+cmake .. && make -j && make install || exit 1
 cd .. || exit 1
 rm -r build
